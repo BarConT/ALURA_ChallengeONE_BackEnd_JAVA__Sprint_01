9e0377f393e66700204ed6832211e837017009ee
BarConT <diego_9deabril@hotmail.com>
2023-03-16 14:12:51 -0300

    Cuadro de diálogo - Menú Principal

--- Spring_01/src/logica/Principal.java
+++ Spring_01/src/logica/Principal.java
@@ -1,5 +1,7 @@
 package logica;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author BarConT
  *
@@ -9,6 +11,17 @@ public class Principal {
 	public static void main(String[] args) {
 
 		System.out.println("Spring 01");
+		String conversor;
+		String [] lista = {"Conversor de moneda", "Conversor de temperatura"};
+		
+		try {
+			conversor = (JOptionPane.showInputDialog(null, "Sellecione una opción de conversión", "Menu", JOptionPane.PLAIN_MESSAGE, null, lista, null).toString());
+		} catch (NullPointerException e) {
+			conversor = null;
+			System.out.println("Fin del programa");
+		}
+		
+		JOptionPane.showMessageDialog(null, "Tu seleción " + conversor);
 
 	}
 
