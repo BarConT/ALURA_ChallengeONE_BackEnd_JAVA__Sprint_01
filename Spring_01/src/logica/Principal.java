31fc60daac7efe3195a06d9372f7bc69557e32aa
BarConT <diego_9deabril@hotmail.com>
2023-03-18 21:12:19 -0300

    Cuadro de diálogo - Opciones de Monedas

--- Spring_01/src/logica/Principal.java
+++ Spring_01/src/logica/Principal.java
@@ -13,6 +13,7 @@ public class Principal {
 		System.out.println("Spring 01");
 		String conversor;
 		String [] lista = {"Conversor de moneda", "Conversor de temperatura"};
+		String [] ListaConversorDeMoneda = {"De Pesos a Dólar", "De Pesos a Euros", "De Pesos a Libras Esterlinas", "De Pesos a Yen Japonés", "De Pesos a Won sul-coreano", "De Dólar a Pesos", "De Euro a Pesos", "De Libras Esterlinas a Pesos", "De Yen Japonés a Pesos", "De Won sul-coreano a Pesos"};
 		
 		try {
 			conversor = (JOptionPane.showInputDialog(null, "Sellecione una opción de conversión", "Menu", JOptionPane.PLAIN_MESSAGE, null, lista, null).toString());
@@ -21,7 +22,16 @@ public class Principal {
 			System.out.println("Fin del programa");
 		}
 		
-		JOptionPane.showMessageDialog(null, "Tu seleción " + conversor);
+		if (conversor.equals(lista[0])) {
+			try {
+				conversor = (JOptionPane.showInputDialog(null, "Eliga la moneda a la que deseas convertir tu dinero", "Monedas", JOptionPane.PLAIN_MESSAGE, null, ListaConversorDeMoneda, null).toString());
+			} catch (NullPointerException e) {
+				conversor = null;
+				System.out.println("Fin del programa");
+			}
+		} else {
+			System.out.println("Fin del programa");
+		}
 
 	}
 
