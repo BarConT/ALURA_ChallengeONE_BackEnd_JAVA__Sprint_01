1aa73dc99743af024d9955cd457ca961017e1d76
BarConT <diego_9deabril@hotmail.com>
2023-03-18 21:39:46 -0300

    Cuadro de diálogo - Entrada de valor y Cuadro de diálogo - Manejo de errores

--- Spring_01/src/logica/Principal.java
+++ Spring_01/src/logica/Principal.java
@@ -10,27 +10,39 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		System.out.println("Spring 01");
-		String conversor;
 		String [] lista = {"Conversor de moneda", "Conversor de temperatura"};
 		String [] ListaConversorDeMoneda = {"De Pesos a Dólar", "De Pesos a Euros", "De Pesos a Libras Esterlinas", "De Pesos a Yen Japonés", "De Pesos a Won sul-coreano", "De Dólar a Pesos", "De Euro a Pesos", "De Libras Esterlinas a Pesos", "De Yen Japonés a Pesos", "De Won sul-coreano a Pesos"};
+		String conversor;
+		String conversorMonedas;
 		
 		try {
 			conversor = (JOptionPane.showInputDialog(null, "Sellecione una opción de conversión", "Menu", JOptionPane.PLAIN_MESSAGE, null, lista, null).toString());
 		} catch (NullPointerException e) {
-			conversor = null;
+			conversor = "";
 			System.out.println("Fin del programa");
 		}
 		
 		if (conversor.equals(lista[0])) {
 			try {
-				conversor = (JOptionPane.showInputDialog(null, "Eliga la moneda a la que deseas convertir tu dinero", "Monedas", JOptionPane.PLAIN_MESSAGE, null, ListaConversorDeMoneda, null).toString());
+				conversorMonedas = (JOptionPane.showInputDialog(null, "Eliga la moneda a la que deseas convertir tu dinero", "Monedas", JOptionPane.PLAIN_MESSAGE, null, ListaConversorDeMoneda, null).toString());
 			} catch (NullPointerException e) {
-				conversor = null;
+				conversorMonedas = "";
 				System.out.println("Fin del programa");
 			}
 		} else {
+			conversorMonedas = "";
 			System.out.println("Fin del programa");
+		}
+		if (!conversorMonedas.isEmpty()) {
+			try {
+				Double cantidadDeDinero = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad de dinero que deseas convertir:"));
+				System.out.println("Cantidad de dinero: $" + cantidadDeDinero);
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Valor no válido");
+			} catch (NullPointerException e) {
+				System.out.println("Fin del programa");
+			}
+			
 		}
 
 	}
